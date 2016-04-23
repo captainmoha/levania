5fdfba4be3f5ad6a24a9c7eb41c453c50b7d20a8
Essam Tarik <essamtarik2000@gmail.com>
2016-04-23 02:24:50 +0200

    added compass

---
+++ plugins/sensors/compass.py
@@ -0,0 +1,22 @@
+import kivy
+kivy.require('1.7.0')
+
+from jnius import autoclass
+from kivy.app import App
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
+from kivy.vector import Vector
+from kivy.animation import Animation
+
+Hardware = autoclass('org.renpy.android.Hardware')
+
+class Compass:
+	output = (0,0,0)
+	def __init__(self):
+		Hardware.magneticFieldSensorEnable(True)
+		Clock.schedule_interval(self.update_compass, 1 / 10.)
+	
+	def update_compass(self, *args):
+		self.output = Hardware.magneticFieldSensorReading()
+		print self.output
+		
